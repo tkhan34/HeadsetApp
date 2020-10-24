9557c371f3f525ea0d0bd54e508382d24663e81f
ADMIN <admin@admins-mbp.lan>
2020-10-23 22:23:12 -0500

    Initial MagicLeap Headset App

---
+++ Assets/RobotTrackingManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+[RequireComponent(typeof(MLPrivilegeRequesterBehavior))]
+
+public class RobotTrackingManager : MonoBehaviour
+{
+    public GameObject TrackedObject; // should show up in inspector
+    private MLPrivilegeRequesterBehavior _privRequester = null;
+
+    void Awake()
+    {
+        _privRequester = GetComponent<MLPrivilegeRequesterBehavior>();
+        _privRequester.OnPrivilegesDone += HandlePrivilegesDone;
+    }
+     
+    void HandlePrivilegesDone(MLResult result)
+    {
+        if(!result.IsOk)
+        {
+            Debug.Log("Error: Priv Request Failed");
+        }
+        else 
+        {
+            Debug.Log("Success: Priv granted");
+            StartCapture();
+        }
+     }
+
+    void StartCapture()
+    {
+        TrackedObject.SetActive(true);
+    }
+}
---
+++ Assets/TrackingBehavior.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+using MagicLeap.Core;
+
+public class TrackingBehavior : MonoBehaviour
+{
+    private bool _targetFound = false;
+    private MLImageTrackerBehavior _trackingBehavior = null;
+    public GameObject digiObject; 
+
+    void Start()
+    {
+        _trackingBehavior = GetComponent<MLImageTrackerBehavior>();
+        _trackingBehavior.OnTargetFound += OnTargetFound;
+        _trackingBehavior.OnTargetLost += OnTargetLost;
+        _trackingBehavior.OnTargetUpdated += OnTargetUpdated;
+
+        RefreshViewMode();
+    }
+
+    void OnTargetFound(MLImageTracker.Target target, MLImageTracker.Target.Result result)
+    {
+        _targetFound = true;
+        RefreshViewMode();
+    }
+
+    void OnTargetLost(MLImageTracker.Target target, MLImageTracker.Target.Result result)
+    {
+        _targetFound = false;
+        RefreshViewMode();
+    }
+
+    void OnTargetUpdated(MLImageTracker.Target target, MLImageTracker.Target.Result result)
+    {
+        transform.position = result.Position;
+        transform.rotation = result.Rotation;
+    }
+
+    void OnDestroy()
+    {
+        _trackingBehavior.OnTargetLost -= OnTargetLost;
+        _trackingBehavior.OnTargetFound -= OnTargetFound;
+        _trackingBehavior.OnTargetUpdated -= OnTargetUpdated;
+    }
+
+    void RefreshViewMode()
+    {
+        if(_targetFound)
+        {
+            digiObject.SetActive(true);
+        }
+        else
+        {
+            digiObject.SetActive(false);
+        }
+    }
+}
